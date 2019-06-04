4e973f06cfecf7b7b9aba31562f5e23e23644269
league <league@administorsimac.attlocal.net>
2019-06-03 18:27:56 -0700

    just started leauge invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,58 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame window = new JFrame();
+			
+public static void main(String[] args) {
+	LeagueInvaders ned = new LeagueInvaders();
+	ned.swingCode();
+	
+}	
+	void swingCode () {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
